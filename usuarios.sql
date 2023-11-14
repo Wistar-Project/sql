e7d0df7a7d7f0029364e1586b7995785863a75c1
fabrisdev <ezepuntocom@hotmail.com>
2023-11-14 18:01:25 -0300

    Usuario de tránsito listo

--- usuarios.sql
+++ usuarios.sql
@@ -52,6 +52,36 @@ GRANT SELECT, INSERT ON lote_formado_por TO "francisco.cazeneuve";
 GRANT SELECT, INSERT ON paquete_asignado_a_pickup TO "francisco.cazeneuve";
 GRANT SELECT, INSERT ON lote_asignado_a_camion TO "francisco.cazeneuve";
 
+/* Api de tránsito */
+GRANT SELECT, INSERT, UPDATE, DELETE ON users TO "wistar-transito";
+GRANT SELECT, INSERT, UPDATE, DELETE ON oauth_clients TO "wistar-transito";
+GRANT SELECT, INSERT, UPDATE, DELETE ON password_resets TO "wistar-transito";
+GRANT SELECT, INSERT, UPDATE, DELETE ON failed_jobs TO "wistar-transito";
+GRANT SELECT, INSERT, UPDATE, DELETE ON personal_access_tokens TO "wistar-transito";
+GRANT SELECT, INSERT, UPDATE, DELETE ON migrations TO "wistar-transito";
+GRANT SELECT, INSERT, UPDATE, DELETE ON oauth_access_tokens TO "wistar-transito";
+GRANT SELECT, INSERT, UPDATE, DELETE ON oauth_auth_codes TO "wistar-transito";
+GRANT SELECT, INSERT, UPDATE, DELETE ON oauth_personal_access_clients TO "wistar-transito";
+GRANT SELECT, INSERT, UPDATE, DELETE ON oauth_refresh_tokens TO "wistar-transito";
+GRANT SELECT, INSERT ON alojamientos TO "wistar-transito";
+GRANT SELECT, INSERT ON sedes TO "wistar-transito";
+GRANT SELECT, INSERT ON personas TO "wistar-transito";
+GRANT SELECT ON personas_roles TO "wistar-transito";
+GRANT INSERT ON administradores TO "wistar-transito";
+GRANT INSERT ON conductores TO "wistar-transito";
+GRANT SELECT, INSERT ON paquetes TO "wistar-transito";
+GRANT SELECT, INSERT ON lotes TO "wistar-transito";
+GRANT INSERT ON vehiculos TO "wistar-transito";
+GRANT INSERT ON lote_formado_por TO "wistar-transito";
+GRANT INSERT ON gerentes TO "wistar-transito";
+GRANT INSERT ON camiones TO "wistar-transito";
+GRANT INSERT ON pickups TO "wistar-transito";
+GRANT SELECT, INSERT, UPDATE, DELETE ON paquete_asignado_a_pickup TO "wistar-transito";
+GRANT SELECT, INSERT, UPDATE, DELETE ON lote_asignado_a_camion TO "wistar-transito";
+GRANT SELECT, INSERT ON lote_formado_por TO "wistar-transito";
+GRANT SELECT, INSERT, DELETE ON conductor_maneja TO "wistar-transito";
+GRANT SELECT ON vehiculos_tipos TO "wistar-transito";
+GRANT INSERT ON funcionarios TO "wistar-transito";
 
 
 /* Api de traducciones */
