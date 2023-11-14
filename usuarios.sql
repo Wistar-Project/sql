a76b76808becbe7b8bcf1ff369f829c6750d936c
fabrisdev <ezepuntocom@hotmail.com>
2023-11-14 17:35:12 -0300

    Usuario api de almacenes listo

--- usuarios.sql
+++ usuarios.sql
@@ -11,7 +11,8 @@ use wistar;
 */
 
 /* api de autenticación */
-GRANT SELECT, INSERT ON users TO "christian.aguirre";
+GRANT SELECT, INSERT, UPDATE, DELETE ON users TO "christian.aguirre";
+GRANT SELECT ON personas_roles TO "christian.aguirre";
 GRANT SELECT, INSERT ON personas TO "christian.aguirre";
 GRANT INSERT ON gerentes TO "christian.aguirre";
 GRANT INSERT ON conductores TO "christian.aguirre";
@@ -27,8 +28,31 @@ GRANT SELECT, INSERT, UPDATE, DELETE ON oauth_auth_codes TO "christian.aguirre";
 GRANT SELECT, INSERT, UPDATE, DELETE ON oauth_personal_access_clients TO "christian.aguirre";
 GRANT SELECT, INSERT, UPDATE, DELETE ON oauth_refresh_tokens TO "christian.aguirre";
 
+/* Api de almacenes */
+GRANT SELECT, INSERT, UPDATE, DELETE ON users TO "francisco.cazeneuve";
+GRANT SELECT, INSERT, UPDATE, DELETE ON oauth_clients TO "francisco.cazeneuve";
+GRANT SELECT, INSERT, UPDATE, DELETE ON password_resets TO "francisco.cazeneuve";
+GRANT SELECT, INSERT, UPDATE, DELETE ON failed_jobs TO "francisco.cazeneuve";
+GRANT SELECT, INSERT, UPDATE, DELETE ON personal_access_tokens TO "francisco.cazeneuve";
+GRANT SELECT, INSERT, UPDATE, DELETE ON migrations TO "francisco.cazeneuve";
+GRANT SELECT, INSERT, UPDATE, DELETE ON oauth_access_tokens TO "francisco.cazeneuve";
+GRANT SELECT, INSERT, UPDATE, DELETE ON oauth_auth_codes TO "francisco.cazeneuve";
+GRANT SELECT, INSERT, UPDATE, DELETE ON oauth_personal_access_clients TO "francisco.cazeneuve";
+GRANT SELECT, INSERT, UPDATE, DELETE ON oauth_refresh_tokens TO "francisco.cazeneuve";
+GRANT INSERT ON administradores TO "francisco.cazeneuve";
+GRANT INSERT ON funcionarios TO "francisco.cazeneuve";
+GRANT INSERT ON personas TO "francisco.cazeneuve";
+GRANT SELECT, INSERT ON alojamientos TO "francisco.cazeneuve";
+GRANT SELECT, INSERT ON sedes TO "francisco.cazeneuve";
+GRANT INSERT ON conductores TO "francisco.cazeneuve";
+GRANT SELECT ON personas_roles TO "francisco.cazeneuve";
+GRANT SELECT, INSERT ON paquetes TO "francisco.cazeneuve";
+GRANT SELECT, INSERT ON lotes TO "francisco.cazeneuve";
+GRANT SELECT, INSERT ON lote_formado_por TO "francisco.cazeneuve";
+GRANT SELECT, INSERT ON paquete_asignado_a_pickup TO "francisco.cazeneuve";
+GRANT SELECT, INSERT ON lote_asignado_a_camion TO "francisco.cazeneuve";
 
-/* fabrizio.cobucci */
 
-/* wistar-traducciones */
+
+/* Api de traducciones */
 GRANT SELECT ON traducciones TO "wistar-traducciones";
